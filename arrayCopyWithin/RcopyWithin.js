78fb0c1e21bc0deb7a372014977ab5d97a952863
zy199226 <122203566@qq.com>
2017-12-19 10:27:40 +0800

    arrayCopyWithin

--- arrayCopyWithin/RcopyWithin.js
+++ arrayCopyWithin/RcopyWithin.js
@@ -6,13 +6,40 @@ Array.prototype.RcopyWithin = function (target, start, end) {
     const O = Object(this);
     const len = O.length >>> 0;
 
+    const relativeTarget = target >> 0;
+    let to = target < 0 ? Math.max(len + relativeTarget, 0) : Math.min(relativeTarget, len);
+
     const relativeStart = start >> 0;
-    const from = start < 0 ? (len + relativeStart) : relativeStart;
+    let from = start < 0 ? Math.max(len + relativeStart, 0) : Math.min(relativeStart, len);
 
-    const relativeEnd = end >> 0;
-    const to = end < 0 ? (len + relativeEnd) : relativeEnd;
+    const relativeEnd = end ? end >> 0 : len;
+    let final = end < 0 ? Math.max(len + relativeEnd, 0) : Math.min(relativeEnd, len);
 
-    if (from >= to) {
-        return this;
+    if (from >= final) {
+        return O;
     }
+
+    let count = Math.min(final - from, len - to);
+
+    let directoin = 1;
+
+    if (from < to && to < (from + count)) {
+        directoin = -1;
+        to += count - 1;
+        from += count - 1;
+    }
+
+    while (count > 0) {
+        if (from in O) {
+            O[to] = O[from];
+        } else {
+            delete O[to];
+        }
+
+        from += directoin;
+        to += directoin;
+        count --;
+    }
+
+    return O;
 };
